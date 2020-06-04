72058cf9a6a3f7ac9c7102dc942db5b204efe43a
yasser <spookyframework@gmail.com>
2020-06-04 19:51:17 +0200

    feat(internals/webpack): Add webpack dev configuration

---
+++ internals/webpack/webpack.dev.js
@@ -0,0 +1,38 @@
+/*
+ * This file is part of the mern-boilerplate project.
+ *
+ * (c) Yasser Ameur El Idrissi <getspookydev@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+const webpack = require('webpack');
+const {outputPath} = require('./paths');
+
+// files regexes
+const cssRegex = /\.(css|scss)$/;
+
+module.exports = {
+  mode: 'development',
+  output: {
+    path: outputPath,
+    filename: 'bundle.js',
+    publicPath: '/',
+  },
+  module: {
+    rules: [
+      {
+        test: cssRegex,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+          },
+          'sass-loader',
+        ],
+      },
+    ],
+  },
+  plugins: [new webpack.HotModuleReplacementPlugin()],
+};
