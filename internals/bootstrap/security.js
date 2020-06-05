94e616624a44ba30a8e011bcc4d4cac7264f06e7
yasser <spookyframework@gmail.com>
2020-06-05 13:20:35 +0200

    feat(internals/security): Block X-XSS-Protection

--- internals/bootstrap/security.js
+++ internals/bootstrap/security.js
@@ -27,3 +27,12 @@ export function hidePoweredBy(req, res, next) {
   res.removeHeader('X-Powered-By');
   next();
 }
+
+// Block X-XSS-Protection.
+export function xssFilter(req, res, next) {
+  // The HTTP X-XSS-Protection response header is a feature of Internet Explorer,
+  // Chrome and Safari that stops pages from loading when they detect reflected cross-site scripting (XSS) attacks
+  // Learn more : https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-XSS-Protection
+  res.setHeader('X-XSS-Protection', '1; mode=block');
+  next();
+}
