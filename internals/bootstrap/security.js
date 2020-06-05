ae4ae2facbb7d311845b903e93f1553db0eaad91
yasser <spookyframework@gmail.com>
2020-06-05 13:26:57 +0200

    refactor(internals/security): Rename Deny X-Frame-Options function

--- internals/bootstrap/security.js
+++ internals/bootstrap/security.js
@@ -38,7 +38,7 @@ export function xssFilter(req, res, next) {
 }
 
 // Deny X-Frame-Options.
-export function frameguard(req, res, next) {
+export function frameGuard(req, res, next) {
   // The X-Frame-Options HTTP response header can be used to indicate whether or not a browser
   // should be allowed to render a page in a <frame>, <iframe>, <embed> or <object> . Sites can use
   // this to avoid clickjacking attacks, by ensuring that their content is not embedded into other sites.
