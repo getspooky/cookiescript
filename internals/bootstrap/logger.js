62b345475d5063fbddd7b118cba635900a3b4a5a
yasser <spookyframework@gmail.com>
2020-06-05 12:41:01 +0200

    feat(internals/bootstrap): Add Logger middleware

---
+++ internals/bootstrap/logger.js
@@ -0,0 +1,43 @@
+/*
+ * This file is part of the mern-boilerplate project.
+ *
+ * (c) Yasser Ameur El Idrissi <getspookydev@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+const chalk = require('chalk');
+const ip = require('ip');
+
+const divider = chalk.gray('\n-----------------------------------');
+
+/**
+ * Logger middleware, you can customize it to make messages more personal
+ */
+const logger = {
+  // Called whenever there's an error on the server we want to print
+  error: err => {
+    console.error(chalk.red(err));
+  },
+
+  // Called when express.js app starts on given port w/o errors
+  appStarted: (port, host, tunnelStarted) => {
+    console.log(`Server started ! ${chalk.green('✓')}`);
+
+    // If the tunnel started, log that and the URL it's available at
+    if (tunnelStarted) {
+      console.log(`Tunnel initialised ${chalk.green('✓')}`);
+    }
+
+    console.log(`
+${chalk.bold('Access URLs:')}${divider}
+Localhost: ${chalk.magenta(`http://${host}:${port}`)}
+      LAN: ${chalk.magenta(`http://${ip.address()}:${port}`) +
+        (tunnelStarted
+          ? `\n    Proxy: ${chalk.magenta(tunnelStarted)}`
+          : '')}${divider}
+${chalk.blue(`Press ${chalk.italic('CTRL-C')} to stop`)}
+    `);
+  },
+};
