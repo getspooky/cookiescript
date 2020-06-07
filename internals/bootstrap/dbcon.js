53ba142a9645872f256b6541072dfc56f2c79e18
yasser <spookyframework@gmail.com>
2020-06-07 15:10:33 +0200

    feat(internals/bootstrap): Setup database connection

---
+++ internals/bootstrap/dbcon.js
@@ -0,0 +1,68 @@
+/*
+ * This file is part of the mern-boilerplate project.
+ *
+ * (c) Yasser Ameur El Idrissi <getspookydev@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+import mongoose from 'mongoose';
+import redis  from 'redis';
+import config from 'internals/utils/config';
+import logger from 'internals/bootstrap/logger';
+
+/*
+|------------------------------------------------------------------------------
+| Open databases connections.
+|------------------------------------------------------------------------------
+|
+| This file is responsible for starting database connections.
+| "mongodb" | "redis"
+|
+*/
+
+// starting databases.
+(async function onDatabaseStart() {
+  // By default mern-boilerplate uses mongodb.
+  const db_default = config('database@default.name');
+  switch (db_default) {
+    case 'mongodb':
+      await onMongodbStart();
+      break;
+    case 'redis':
+      await onRedisStart();
+      break;
+  }
+})();
+
+
+// starting MongoDB database.
+async function onMongodbStart() {
+  const driver = 'mongodb';
+  const db_host = config('database@connections.mongodb.db_host');
+  const db_name = config('database@connections.mongodb.db_name');
+  // If you need to create additional connections, use mongoose.createConnection.
+  const connecting = await  connect(`mongodb://${db_host}/${db_name}`,
+  {
+    useFindAndModify,
+    useCreateIndex,
+    useNewUrlParser,
+    useUnifiedTopology: true,
+  });
+  if(connecting) {
+    logger.dbStarted(driver,db_name);
+  }
+}
+
+// starting Redis database.
+async function onRedisStart() {
+  const driver = 'redis';
+  const db_expiration = config('database@redis.expiration');
+  // redis can be accessed anywhere.
+  // emit connect as soon as the stream is connected to the server.
+  redis.on('connect', () => {
+    logger.dbStarted(driver, null);
+  });
+}
+
