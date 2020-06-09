489e3ba96cad6ea9fcaf70251184266efa22cd8b
yasser <spookyframework@gmail.com>
2020-06-09 15:09:56 +0200

    feat(internals/utils): Add logging utils

---
+++ internals/utils/logging.js
@@ -0,0 +1,27 @@
+/*
+ * This file is part of the mern-boilerplate project.
+ *
+ * (c) Yasser Ameur El Idrissi <getspookydev@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+import path from 'path';
+import winston from 'winston';
+import config from 'internals/utils/config';
+
+const file = config('logging@file');
+const clog = config('logging@console');
+const exitOnError = config('logging@exitOnError');
+
+// Log args into .keep file
+export function log(...args) {
+  const logger = new winston.Logger({
+    transports: [
+      new winston.transports.File(file),
+      new winston.transports.Console(clog)
+    ],
+    exitOnError
+  });
+}
