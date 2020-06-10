93e4c90dac59addb36ca1ece090ec8936d74d457
yasser <spookyframework@gmail.com>
2020-06-10 16:06:20 +0200

    feat(database/schemas): Add User Schema mongo

---
+++ database/schemas/2020_06_08_create_user_schema.js
@@ -0,0 +1,57 @@
+/*
+ * This file is part of the mern-boilerplate project.
+ *
+ * (c) Yasser Ameur El Idrissi <getspookydev@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+import mongoose from 'mongoose';
+import config from 'internals/utils/config';
+
+const expires = config('auth@passwords.expires');
+const token_size = config('auth@passwords.token_size');
+
+/* User Schema 2020_06_08_create_user_schema */
+const CreateUserSchema = mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      minLength: 10,
+    },
+    email: {
+      type: String,
+      required: true,
+      minLength: 5,
+      validate: {
+        validator(v) {
+          return /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/.test(v);
+        },
+      },
+    },
+    password: {
+      type: String,
+      required: true,
+      minLength: 10,
+    },
+    email_verified_at: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
+    // Reset Password Schema
+    reset: {
+      token: {
+        type: String,
+        required: false,
+        length: token_size,
+        expires,
+      },
+    },
+  },
+  {timestamps: true}
+);
+
+export default CreateUserSchema;
