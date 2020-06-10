37ab20a8bc41ddfd0fd300ac8ace673dfd80d1e2
yasser <spookyframework@gmail.com>
2020-06-10 20:01:44 +0200

    break(app): Remove App.test.js

--- app/App.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import App from './App';
-
-it('renders without crashing', () => {
-  const wrapper = shallow(<App />);
-  expect(wrapper.length).toBe(1);
-});
