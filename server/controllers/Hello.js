ff4b2f1bee763a0e6936a433ae6054d02ac6e1b6
yasser <spookyframework@gmail.com>
2020-06-11 20:32:10 +0200

    feat(server/controller): Add Hello controller

---
+++ server/controllers/Hello.js
@@ -0,0 +1,12 @@
+/*
+ * This file is part of the mern-boilerplate project.
+ *
+ * (c) Yasser Ameur El Idrissi <getspookydev@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+export function SayHello(req,res,next) {
+  res.send('👋 Welcome to <b>mern-boilerplate</b>');
+};
