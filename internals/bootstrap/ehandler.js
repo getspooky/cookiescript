23526217a9171823ba7903e723ae67f1c199b2a2
yasser <spookyframework@gmail.com>
2020-06-11 21:00:42 +0200

    feat(internals/bootstrap): Add bootstrap ehandler

---
+++ internals/bootstrap/ehandler.js
@@ -0,0 +1,22 @@
+/*
+ * This file is part of the mern-boilerplate project.
+ *
+ * (c) Yasser Ameur El Idrissi <getspookydev@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+import logger from 'internals/utils/logging';
+
+// Handling Errors.
+export default function({message, statusCode, fileName}, req, res, next) {
+  // If err has no specified error code, set error code to 'Internal Server Error (500)'
+  if (!statusCode) statusCode = 500;
+  // If we're in development mode then log to the `console-handler` with the format:
+  // ${timestamp} ${level}: ${message}
+  if (process.env.NODE_ENV === 'development') logger.log(message, statusCode);
+  return res.status(statusCode).json({
+    errors: {message, fileName},
+  });
+}
