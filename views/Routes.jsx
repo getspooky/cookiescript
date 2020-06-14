d3b238591c1d9edd84898684959dab3e1b20343a
yasser <spookyframework@gmail.com>
2020-06-14 21:03:00 +0200

    feat(views): Add Welcome, Register, Login path

---
+++ views/Routes.jsx
@@ -0,0 +1,33 @@
+/*
+ * This file is part of the CookieScript project.
+ *
+ * (c) Yasser Ameur El Idrissi <getspookydev@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+import Welcome from './containers/Welcome.container.jsx';
+import Register from './containers/auth/Register.container.jsx';
+import Login from './containers/auth/Login.container';
+
+// registering all routes.
+const Routes = [
+  {
+    path: '/',
+    name: 'welcome',
+    component: Welcome,
+  },
+  {
+    path: '/register',
+    name: 'register',
+    component: Register,
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: Login,
+  },
+];
+
+export default Routes;
