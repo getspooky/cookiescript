74eeb00f43110fa7e7c3491f57c12c93b6263f04
yasser <spookyframework@gmail.com>
2020-06-16 21:03:54 +0200

    break(config): Remove all js files from config folder

--- config/hashing.js
+++
@@ -1,6 +0,0 @@
-import {env} from 'support/helpers/common';
-
-export default {
-  // Here you may specify the configuration options that should be used when passwords are hashed.
-  salt: env('PASSWORD_HASH_SALT'),
-};
--- config/jwt.js
+++
@@ -1,8 +0,0 @@
-import {env} from 'support/helpers/common';
-
-export default {
-  // eslint-disable-next-line no-tabs
-  algorithm: 'RS256', // HS256	, HS384 , HS512 , RS256 , RS384 , RS512 , PS256 , PS384 , PS512 , ES256 , ES384 , ES512
-  private_Key: env('JWT_SECRET_KEY'),
-  expiration: '5h',
-};
