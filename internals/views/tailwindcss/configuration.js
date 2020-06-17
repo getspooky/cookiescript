2698b7b8e06ddf5177cd978b18a714359bf48ac3
yasser <spookyframework@gmail.com>
2020-06-17 12:07:57 +0200

    feat(internals/views/tailwindcss): Add tailwindcss configuration file

---
+++ internals/views/tailwindcss/configuration.js
@@ -0,0 +1,18 @@
+/*
+ * This file is part of the CookieScript project.
+ *
+ * (c) Yasser Ameur El Idrissi <getspookydev@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+module.exports = {
+  theme: {
+    extend: {
+      //
+    },
+  },
+  variants: {},
+  plugins: [],
+}
