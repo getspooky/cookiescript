cad16cb4083a52abbbd2bdbbc046a17adcec598a
yasser <spookyframework@gmail.com>
2020-06-18 00:04:31 +0200

    feat(internals/bootstrap): Add routes wrapper

--- internals/bootstrap/routes.js
+++ internals/bootstrap/routes.js
@@ -1,5 +1,5 @@
 /*
- * This file is part of the mern-boilerplate project.
+ * This file is part of the CookieScript project.
  *
  * (c) Yasser Ameur El Idrissi <getspookydev@gmail.com>
  *
@@ -19,7 +19,7 @@ const Route = express.Router();
 | Register Routes
 |--------------------------------------------------------------------------
 |
-| Here is where all mern-boilerplate routes are build.
+| Here is where all CookieScript routes are build.
 | To add new routes feel free to open the routes.yml file located in
 | path => server/Routes.yml
 */
@@ -28,13 +28,14 @@ const Route = express.Router();
   const routes = parseYmlRoutes();
   for (let [key, value] of Object.entries(routes)) {
     let method = 'get';
-    let validator  = () => (req,res,next) => next();
+    let validate  = () => (req,res,next) => next();
     const middlewares = [];
+    // check yml stracture
     if(!value.hasOwnProperty('controller') || !value.hasOwnProperty('path')) {
       throw new TypeError('controller or path is required');
     }
-    if(typeof value.controller !== 'function') {
-      throw new TypeError('controller must be function');
+    if(typeof value.controller !== 'string') {
+      throw new TypeError('controller must be string');
     }
     // check if method exists if not we use GET by default.
     // push middlewares and validator
@@ -42,15 +43,19 @@ const Route = express.Router();
       method = value.method;
     }
     if(value.hasOwnProperty('middlewares')) {
-       middlewares.push(value.controller);
+       const { path: path_middleware, fun: fun_middleware } = extractFunPath(value.controller);
+       const new_middleware_path = require(path.resolve(__dirname,'../../', path_middleware));
+       middlewares.push(new_middleware_path[fun_middleware]);
     }
     if(value.hasOwnProperty('validator')) {
-      validator = value.validator;
+       validate = require(path.resolve(__dirname, '../../', value.validator)).Validator;
     }
-    const scan_controller = value.controller.split('@');
-    const new_controller_path = require(path.resolve(__dirname,'../../', scan_controller[0]));
+    // extract controller path and function
+    const { path: path_controller, fun: fun_controller } = extractFunPath(value.controller);
+    const new_controller_path = require(path.resolve(__dirname,'../../', path_controller));
+
     // init routes
-    Route[method](value.path, ...middlewares ,validator() , new_controller_path[scan_controller[1]]);
+    Route[method](value.path,...middlewares ,validate() ,new_controller_path[fun_controller]);
   }
 })();
 
@@ -60,6 +65,15 @@ function parseYmlRoutes() {
   const fileContents = fs.readFileSync(routes_content, 'utf8');
   const data = yaml.safeLoad(fileContents);
   return data;
+}
+
+// extract function and path
+function extractFunPath(value) {
+  const extract_data = value.split('@');
+  return {
+    path: extract_data[0],
+    fun: extract_data[1]
+  }
 }
 
 export default Route;
