1086e40096b1d51243f6961117275751e8093fd9
yasser <spookyframework@gmail.com>
2020-06-19 00:09:17 +0200

    break(server): Remove environment file

--- server/environment.js
+++
@@ -1,8 +0,0 @@
-import path from 'path';
-import env from 'dotenv';
-
-// Load environment variables from .env file
-env.config({
-  encoding: 'utf8',
-  path: path.resolve(__dirname, '../.env'),
-});
