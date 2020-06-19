e9ecdf246831f74e402d9a9a531f6bff368ae4a3
yasser <spookyframework@gmail.com>
2020-06-19 18:46:31 +0200

    test(server): Testing /api/v1/hello

---
+++ server/__tests__/Hello.spec.js
@@ -0,0 +1,46 @@
+/*
+ * This file is part of the CookieScript project.
+ *
+ * (c) Yasser Ameur El Idrissi <getspookydev@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+import request from 'supertest';
+import app from 'server/App';
+import {
+  SayHello
+} from 'server/controllers/Hello';
+
+var url = '/api/v1/hello';
+
+beforeAll(function () {
+  // adding /hello route
+  app.use(url, SayHello);
+});
+
+describe('Testing GET /', function () {
+
+  it('should return 200', done => {
+    request(app)
+      .get(url)
+      .expect(200)
+      .end(function (err, res) {
+        if (err) return done(err);
+        done();
+      });
+  });
+
+  it('check Content-Type', done => {
+    request(app)
+      .get(url)
+      .set('Accept', 'application/json')
+      .expect('Content-Type', "text/html; charset=utf-8")
+      .end(function (err, res) {
+        if (err) return done(err);
+        done();
+      });
+  });
+
+});
