28f557a3ddaf90333279b4a97201e72e3892852e
yasser <spookyframework@gmail.com>
2020-06-19 18:38:27 +0200

    fix: Cannot find setupFiles path

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,18 @@
+/*
+ * This file is part of the mern-boilerplate project.
+ *
+ * (c) Yasser Ameur El Idrissi <getspookydev@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 module.exports = {
   roots: ['./'],
-  testRegex: '\\.(test|spec)\\.js$',
+  testRegex: '\\.(test|spec)\\.jsx?$',
   moduleFileExtensions: ['js', 'jsx'],
   testPathIgnorePatterns: ['/(build|docs|node_modules|packages)/'],
-  setupFiles: ['./setupTests.js'],
+  setupFiles: ['./internals/testing/enzyme.js'],
   collectCoverage: true,
   coverageReporters: ['json', 'lcov'],
   transform: {
