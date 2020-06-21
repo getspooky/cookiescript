4dcff60a749441b0e8ca061ae89d3e1d543f431a
yasser <spookyframework@gmail.com>
2020-06-22 00:26:30 +0200

    feat(internals/views): Create Browser History

---
+++ internals/views/history.js
@@ -0,0 +1,12 @@
+/*
+ * This file is part of the CookieScript project.
+ *
+ * (c) Yasser Ameur El Idrissi <getspookydev@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+import history from 'history';
+const createBrowserHistory = history.createBrowserHistory();
+export default createBrowserHistory;
