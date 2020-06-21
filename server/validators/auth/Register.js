2e7414324b0f6fbe8528ed3398ace9fc18bdea49
yasser <spookyframework@gmail.com>
2020-06-21 12:29:28 +0200

    fix(server/validators/auth): Remove Campare password from register

--- server/validators/auth/Register.js
+++ server/validators/auth/Register.js
@@ -7,42 +7,33 @@
  * file that was distributed with this source code.
  */
 
-import { check } from 'express-validator';
+import {
+  check
+} from 'express-validator';
 
 // Get the validation rules that apply to the request.
 export function Validator() {
   return [
     check('username')
-      .not()
-      .isEmpty()
-      .withMessage('Username is missing')
-      .isLength({
-        min: 10,
-      })
-      .withMessage('Username must have more than 10 characters'),
+    .not()
+    .isEmpty()
+    .withMessage('Username is missing')
+    .isLength({
+      min: 10,
+    })
+    .withMessage('Username must have more than 10 characters'),
     check('email')
-      .not()
-      .isEmpty()
-      .withMessage('Email is missing')
-      .isEmail()
-      .withMessage('Email is not valid'),
+    .not()
+    .isEmpty()
+    .withMessage('Email is missing')
+    .isEmail()
+    .withMessage('Email is not valid'),
     check('password')
-      .not()
-      .isEmpty()
-      .withMessage('Password is missing')
-      .isLength({
-        min: 10,
-      })
-      .withMessage('Password must have more than 10 characters')
-      .custom((value, {req, loc, path}) => {
-        const {confirm_password} = req.body;
-        if (value !== confirm_password) {
-          // trow error if passwords do not match
-          throw new TypeError(
-            "Passwords don't match ! Please Try Again"
-          );
-        }
-        return value;
-      }),
+    .not()
+    .isEmpty()
+    .withMessage('Password is missing')
+    .isLength({
+      min: 10,
+    }),
   ];
 }
