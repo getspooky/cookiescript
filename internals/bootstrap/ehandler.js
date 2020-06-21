a82e01ccc6b0b45c341f7ef5d3116327542e396d
yasser <spookyframework@gmail.com>
2020-06-21 19:37:44 +0200

    fix(internals/bootstrap): Get status from err object

--- internals/bootstrap/ehandler.js
+++ internals/bootstrap/ehandler.js
@@ -10,13 +10,20 @@
 import logger from 'internals/utils/logging';
 
 // Handling Errors.
-export default function({message, statusCode, fileName}, req, res, next) {
+export default function ({
+  message,
+  status,
+  fileName
+}, req, res, next) {
   // If err has no specified error code, set error code to 'Internal Server Error (500)'
-  if (!statusCode) statusCode = 500;
+  if (!status) status = 500;
   // If we're in development mode then log to the `console-handler` with the format:
   // ${timestamp} ${level}: ${message}
-  if (process.env.NODE_ENV === 'development') logger.log(message, statusCode);
-  return res.status(statusCode).json({
-    errors: {message, fileName},
+  if (process.env.NODE_ENV === 'development') logger.log(message, status);
+  return res.status(status).json({
+    errors: {
+      message,
+      fileName
+    },
   });
 }
