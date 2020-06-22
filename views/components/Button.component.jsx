0bd5fd930deb16b4ed7691e4fce15abff2c7519d
yasser <spookyframework@gmail.com>
2020-06-22 16:05:23 +0200

    feat(views/components): Add Button Component

---
+++ views/components/Button.component.jsx
@@ -0,0 +1,32 @@
+/*
+ * This file is part of the CookieScript project.
+ *
+ * (c) Yasser Ameur El Idrissi <getspookydev@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Button(props) {
+  const { text, color, handle } = props;
+  return (
+    <button
+      className={`bg-transparent hover:bg-${color}-500 text-${color}-700 font-semibold hover:text-white py-2 px-4 border border-${color}-500 hover:border-transparent rounded`}
+      onClick={handle}
+    >
+      {text}
+    </button>
+  );
+}
+
+Button.propTypes = {
+  text: PropTypes.string.isRequired,
+  color: PropTypes.string.isRequired,
+  handle: PropTypes.func.isRequired,
+};
+
+export default Button;
+
