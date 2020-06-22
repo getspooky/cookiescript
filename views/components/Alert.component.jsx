563c88c983288217480b3cfd5c8125baa2e3d9b6
yasser <spookyframework@gmail.com>
2020-06-22 13:10:08 +0200

    feat(views/components): Add Alert component

---
+++ views/components/Alert.component.jsx
@@ -0,0 +1,29 @@
+/*
+ * This file is part of the CookieScript project.
+ *
+ * (c) Yasser Ameur El Idrissi <getspookydev@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Alert(props) {
+  const { className, message, details } = props;
+  return (
+    <div className={`${className} border-l-4 p-4`}>
+      <div className="header">{message}</div>
+      <p>{details}</p>
+    </div>
+  );
+}
+
+Alert.propTypes = {
+  className: PropTypes.string.isRequired,
+  message: PropTypes.string.isRequired,
+  details: PropTypes.string.isRequired,
+};
+
+export default Alert;
