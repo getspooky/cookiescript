f4650a8d81f3c907cbcfc22d26428a4ecb76b6f4
yasser <spookyframework@gmail.com>
2020-06-24 01:09:58 +0200

    fix(views): Cannot find i18n module

---
+++ views/index.jsx
@@ -0,0 +1,16 @@
+/*
+ * This file is part of the CookieScript project.
+ *
+ * (c) Yasser Ameur El Idrissi <getspookydev@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import 'internals/views/i18n';
+import 'internals/views/tailwindcss/build.css';
+
+ReactDOM.render(<App />, document.getElementById('root'));
