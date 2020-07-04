14e073ed96247ed1bf7bfa82963f37088ff25e3a
yasser <spookyframework@gmail.com>
2020-07-04 12:39:22 +0200

    feat(internals/utils/exceptions): Export all exception

---
+++ internals/utils/exceptions/index.js
@@ -0,0 +1,15 @@
+/*
+ * This file is part of the CookieScript project.
+ *
+ * (c) Yasser Ameur El Idrissi <getspookydev@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+export * from './bad-request';
+export * from './conflict';
+export * from './exception';
+export * from './forbidden';
+export * from './unauthorized';
+export * from './unprocessable-entity';
