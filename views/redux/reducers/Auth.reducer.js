dee085abd62de46780073d493bdd6863ec3d000a
yasser <spookyframework@gmail.com>
2020-07-04 23:09:39 +0200

    feat(views/redux/reducers): Add Auth reducers

---
+++ views/redux/reducers/Auth.reducer.js
@@ -0,0 +1,41 @@
+/*
+ * This file is part of the mern-boilerplate project.
+ *
+ * (c) Yasser Ameur El Idrissi <getspookydev@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+import { AUTH_REGISTER, AUTH_LOGIN, AUTH_LOGOUT } from 'views/global-vars';
+
+// initial state.
+const initState = {
+  Auth: {
+    isAuthenticated: false,
+    // Get token from localStorage.
+    token: localStorage.getItem('_token'),
+  },
+};
+
+// Auth Reducer.
+function authReducer(state = initState, {type, payload}) {
+  switch (type) {
+    case AUTH_REGISTER:
+    case AUTH_LOGIN:
+      state = {
+        Auth: { isAuthenticated: true, token: payload.data },
+      };
+      break;
+    case AUTH_LOGOUT:
+      // Remove token from localStorage.
+      localStorage.removeItem('_token');
+      state = {
+        Auth: { isAuthenticated: false, token: null },
+      };
+      break;
+  }
+  return state;
+}
+
+export default authReducer;
