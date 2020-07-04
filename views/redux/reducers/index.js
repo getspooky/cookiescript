5f99ff56e1f3b775f0cd8a8ee4e478c45f571c73
yasser <spookyframework@gmail.com>
2020-07-04 23:10:36 +0200

    feat(views/redux/reducers): Combine all reducers

---
+++ views/redux/reducers/index.js
@@ -0,0 +1,22 @@
+/*
+ * This file is part of the mern-boilerplate project.
+ *
+ * (c) Yasser Ameur El Idrissi <getspookydev@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+import {
+  combineReducers
+} from 'redux';
+import AuthReducer from './Auth.reducer';
+import ProfileReducer from './Profile.reducer';
+
+// Combine all reducers
+const rootReducer = combineReducers({
+  AUTH_REDUCER: AuthReducer,
+  PROFILE_REDUCER: ProfileReducer
+});
+
+export default rootReducer;
