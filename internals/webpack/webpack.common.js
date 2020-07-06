38a6314b820109c8d9768ef9e535b1ea70262b7b
yasser <spookyframework@gmail.com>
2020-07-06 19:41:21 +0200

    fix(internals/webpack): Cannot find dotenv-webpack

--- internals/webpack/webpack.common.js
+++ internals/webpack/webpack.common.js
@@ -9,7 +9,6 @@
 
 const path = require('path');
 const webpack = require('webpack');
-const Dotenv = require('dotenv-webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 //
 const htmlTemplate = path.resolve(__dirname, '../../', 'public', 'index.html');
