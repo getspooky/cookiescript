6f8209752ce9fd6689280b1117764ce9d28a56f8
yasser <spookyframework@gmail.com>
2020-07-07 00:07:52 +0200

    feat(views/redix/actions): Export all actions used

---
+++ views/redux/actions/index.js
@@ -0,0 +1,11 @@
+/*
+ * This file is part of the mern-boilerplate project.
+ *
+ * (c) Yasser Ameur El Idrissi <getspookydev@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+export * from './Auth.action';
+export * from './Profile.action';
