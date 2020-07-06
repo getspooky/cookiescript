e4404a6a7bfb7b12bdef8d5b4616bf6ea8a6e1b0
yasser <spookyframework@gmail.com>
2020-07-07 00:05:49 +0200

    fix(views/services): Add Content-Type header

--- views/services/Profile.service.js
+++ views/services/Profile.service.js
@@ -18,6 +18,7 @@ export async function HTTP_REQUEST_PROFILE(token) {
     method: 'GET',
     headers: {
       Authorization: `Bearer ${token}`,
+      'Content-Type': 'application/json'
     },
   });
 }
