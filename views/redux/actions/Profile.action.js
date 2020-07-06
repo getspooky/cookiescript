07acdc6e259c0b85c22ea0bb3d4ec4dcc8141022
yasser <spookyframework@gmail.com>
2020-07-06 22:04:55 +0200

    feat(views/redux/actions): Add Profile action

---
+++ views/redux/actions/Profile.action.js
@@ -0,0 +1,28 @@
+/*
+ * This file is part of the mern-boilerplate project.
+ *
+ * (c) Yasser Ameur El Idrissi <getspookydev@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+import {
+  HTTP_REQUEST_PROFILE
+} from 'views/services/Profile.action';
+import {
+  LOAD_PROFILE
+} from 'views/global-vars';
+
+// Profile Action.
+export function profileAction() {
+  return async (dispatch, getState) => {
+    const token = getState().Auth.token;
+    const data = await HTTP_REQUEST_PROFILE(token);
+    // Dispatch Action.
+    dispatch({
+      type: LOAD_PROFILE,
+      payload: data
+    });
+  };
+}
